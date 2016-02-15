af2feb2b0d1ea410813337e99a9d1d11432c9495
EthanJamesLew <ethan.lew@pcc.edu>
2016-02-14 19:49:29 -0800

    Changed user feedback

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	string fileName;
+	string fileName = "";
 	ifstream inFile;
 
 	do {
@@ -24,10 +24,13 @@ int main(int argc, char *argv[])
 
 	fileData::Counter count = fileData::Counter(inFile);
 	cout << "Filename: " << fileName << endl;
-	cout << "\nNumber of words: " << count.getNumWords() << endl;
+	cout << "\nNumber of uppercase letters: " << count.getNumUpper() << endl;
+	cout << "\nNumber of lowercase letters: " << count.getNumLower() << endl;
 	cout << "\nNum of characters: " << count.getNumChars() << endl;
-	cout << "\nNum of lines with 1 digit and 1 lower case: " <<count.getNumDigits() << endl;
-	cout << "\nNum of lines with 2 digits and 3 lower-case: " << count.getNumDigits2() << endl;
+	cout << "\nNumber of digits: " << count.getNumDigits() << endl;
+	cout << "\nNumber of words: " << count.getNumWords() << endl;
+	cout << "\nNum of lines with 1 digit and 1 lower case: " <<count.getCond() << endl;
+	cout << "\nNum of lines with 2 digits and 3 lower-case: " << count.getCond2() << endl;
 
 	cout << endl << "\n Wrote file to disk." << endl;
 
